64315fa3eac31e1ae71f92477c5deba26899ea81
KacperPodolec <162919635+KacperPodolec@users.noreply.github.com>
2025-08-25 12:11:11 +0200

    Utworzenie projektu
    Utworzenie projektu Windows Forms i konfiguracja testowego połączenia do bazy danych.

---
+++ EDziennik/Data/Db.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Npgsql;
+using System.Windows.Forms;
+
+namespace EDziennik.Data
+{
+    public static class Db
+    {
+        public static NpgsqlConnection GetConnection()
+        {
+            string connString = ConfigurationManager
+                .ConnectionStrings["EdziennikDb"].ConnectionString;
+            MessageBox.Show($"Connection string: {connString}");
+
+            return new NpgsqlConnection(connString);
+        }
+    }
+}
